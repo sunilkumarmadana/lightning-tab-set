18a4c170631479f1bbae8cfcbdfdce33220d866e
SUNIL MADANA <sunil@marketing-automation.ca>
2021-07-21 01:38:11 -0700

    Initial commit

--- force-app/main/default/aura/AuraTabSet/AuraTabSetHelper.js
+++
@@ -1,9 +0,0 @@
-({
-    fireComponentEvent : function(cmp, event, messageContent) {
-        // Get the component event by using the
-        // name value from aura:registerEvent
-        var cmpEvent = cmp.getEvent("cmpEvent");
-        cmpEvent.setParams({ "message" : messageContent });
-        cmpEvent.fire();
-    }
-})
